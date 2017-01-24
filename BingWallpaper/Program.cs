0962eb0a8e16feaac7e888c29f9e2359e762c3de
Yemi <zcyemi@gmail.com>
2017-01-25 00:12:21 +0800

    first commit

---
+++ BingWallpaper/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Xml;
+
+namespace BingWallpaper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri uriBing = new Uri("http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1");
+            WebRequest webRequest = WebRequest.Create(uriBing);
+            WebResponse webResponse = webRequest.GetResponse();
+            Stream stream = webResponse.GetResponseStream();
+
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load(stream);
+
+
+            string picturePath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            string wallpaperSavePath = picturePath + "\\Bing\\";
+
+            //check folder
+            if(!Directory.Exists(wallpaperSavePath))
+            {
+                Directory.CreateDirectory(wallpaperSavePath);
+            }
+
+            string fullStartDate = xmldoc["images"]["image"]["fullstartdate"].InnerText;
+            string urlBase = xmldoc["images"]["image"]["urlBase"].InnerText;
+
+            string curImagePath = wallpaperSavePath + fullStartDate + ".jpg";
+
+            if (File.Exists(curImagePath))
+            {
+                Console.WriteLine("skip download!");
+            }
+            else
+            {
+                string downloadUrl = "http://www.bing.com" + urlBase + "_1920x1080.jpg";
+                WebClient webClient = new WebClient();
+
+                try
+                {
+                    webClient.DownloadFile(downloadUrl, @curImagePath);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Wallpaper.SetWallPaper(curImagePath);
+
+            Console.WriteLine("done!");
+
+            Environment.Exit(0);
+        }
+
+    }
+}
---
+++ BingWallpaper/Wallpaper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace BingWallpaper
+{
+    public class Wallpaper
+    {
+
+        public static void SetWallPaper(string filename)
+        {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, filename,SPIF_UPDATEINIFILE);
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SystemParametersInfo(UInt32 uiAction, UInt32 uiParam, String pvParam, UInt32 fWinIni);
+        private static UInt32 SPI_SETDESKWALLPAPER = 20;
+        private static UInt32 SPIF_UPDATEINIFILE = 0x1;
+
+    }
+
+}
